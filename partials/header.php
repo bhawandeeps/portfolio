9af4007c80cc346087d8cc66219bd1ebc5eddf87
bhawandeep <bhawandeeps@protonmail.com>
2025-10-22 22:06:54 +0530

    edited headers

--- partials/header.php
+++ partials/header.php
@@ -4,7 +4,8 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Bhawandeep Singh | Web Developer</title>
-        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/style.css">        
+        <link rel="icon" type="image/x-icon" href="img/favicon.ico">
     </head>
     <body>
 
