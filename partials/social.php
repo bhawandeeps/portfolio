d4b16b6e3847447415ab45692854ae545fba2254
bhawandeep <bhawandeeps@protonmail.com>
2025-10-23 00:15:28 +0530

    updated links.

--- partials/social.php
+++ partials/social.php
@@ -1,8 +1,9 @@
 <div class="socials"> 
     <ul class="social-links"> 
         <li><a href="https://github.com/pandorassss" target="_blank">GitHub</a></li> 
-        <li><a href="https://linkedin.com/in/" target="_blank">LinkedIn</a></li> 
-        <li><a href="https://twitter.com/" target="_blank">Twitter</a></li> 
+        <li><a href="https://www.linkedin.com/in/bhawandeep-singh-5613ba377/" target="_blank">LinkedIn</a></li> 
+        <li><a href="https://x.com/bhawandeepsi" target="_blank">Twitter</a></li> 
+
         <li><a href="mailto:bhawandeeps@protonmail.com">Email</a></li> 
     </ul> 
 </div>
